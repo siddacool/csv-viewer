f8a310fd2e11a7e0cee9f71b6776c4d0571be80e
Siddhesh Mangela <sidthecool007@gmail.com>
2022-03-22 15:31:05 +0530

    pwa added

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,41 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'CSV Viewer',
+        short_name: 'CSV Viewer',
+        description: 'CSV Viewer',
+        theme_color: '#fff',
+        display: 'standalone',
+        background_color: '#fff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
