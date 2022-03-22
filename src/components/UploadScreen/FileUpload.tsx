c22825a0c4e256bd5b9a516935986c4a3e051528
Siddhesh Mangela <sidthecool007@gmail.com>
2022-03-22 10:41:29 +0530

    file uploader added

--- src/components/UploadScreen/FileUpload.tsx
+++ src/components/UploadScreen/FileUpload.tsx
@@ -1,10 +1,22 @@
 import { Component } from 'solid-js';
+import { addCsvData } from '~/store';
 import styles from './style.module.scss';
 
 const FileUpload: Component = () => {
+  const onFileUpload = async (e: { target: { files: any[] } }) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function () {
+      addCsvData(reader.result as string);
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
   return (
     <div class={styles.FileUpload}>
-      <input type="file" accept=".csv, text/plain" id="csv-upload" />
+      <input type="file" accept=".csv, text/plain" id="csv-upload" onChange={onFileUpload} />
       <label for="csv-upload">Upload</label>
     </div>
   );
