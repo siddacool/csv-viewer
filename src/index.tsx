6694fc961fb148bc362b09c38e3cf40c5b2c8972
Siddhesh Mangela <sidthecool007@gmail.com>
2022-03-24 14:47:52 +0530

    serviceWorker PWA added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
+import { registerSW } from 'virtual:pwa-register';
 
 import './index.css';
 import App from './App';
+
+if ('serviceWorker' in navigator) {
+  registerSW({
+    onNeedRefresh() {},
+    onOfflineReady() {},
+  });
+}
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
